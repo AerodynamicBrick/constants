881f254e7b5a3e193004bd5efde2b58eebdb565a
AerodynamicBrick <aerothebrick@gmail.com>
2018-10-03 11:18:27 -0400

    Revert "constants work!"
    This reverts commit 8191e1d65d6701f760399bf707dd837e6b0426ca.

--- Line.java
+++ Line.java
@@ -31,17 +31,18 @@ public class Line
 		java.lang.reflect.Method method;
 		try
 		{
-			//if(this.op.equals("constant")) {psu.constant(operands);}
-			if(!this.op.equals(""))
+			if(this.op!="")
 			{
 				method = psu.getClass().getMethod(this.op, ArrayList.class); //psu.op(operands)
 				this.instructionBin = (String) method.invoke(psu, operands);
+				
 				//if you didn't catch by here than the op is not recognized.
 				isRecognisedOp=true;
 			}
 		}
-		catch (Exception e){isRecognisedOp=false;}
+		catch (Exception e) {isRecognisedOp=false;}
 	}
+
 	private void decode(String lin)
 	{
 		String linarr[] = {"","","",""};
